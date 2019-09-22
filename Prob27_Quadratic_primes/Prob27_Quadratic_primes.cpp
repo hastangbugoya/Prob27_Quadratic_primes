4d0061f4c9cb8e275a5e83554fa9727f247f3f9a
Jeffrey Joseph <jjrzacal@outlook.com>
2019-09-22 04:24:10 -0700

    Add project files.

---
+++ Prob27_Quadratic_primes/Prob27_Quadratic_primes.cpp
@@ -0,0 +1,41 @@
+﻿// Prob27_Quadratic_primes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*
+Euler discovered the remarkable quadratic formula:
+
+n^2+n+41
+It turns out that the formula will produce 40 primes for the consecutive integer values 0≤n≤39. However, when n=40,40^2+40+41=40(40+1)+41 is divisible by 41, and certainly when n=41,412+41+41 is clearly divisible by 41.
+
+The incredible formula n^2−79n+1601 was discovered, which produces 80 primes for the consecutive values 0≤n≤79. The product of the coefficients, −79 and 1601, is −126479.
+
+Considering quadratics of the form:
+
+n^2+an+b, where |a|<1000 and |b|≤1000
+
+where |n| is the modulus/absolute value of n
+e.g. |11|=11 and |−4|=4
+Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
+
+::: make the sieve ...1 to 2000, just to be sure
+do the equation while the result of the quardatic formula is less than 2000...
+
+*/
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
